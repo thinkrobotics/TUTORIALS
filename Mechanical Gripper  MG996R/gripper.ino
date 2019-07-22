55d29c5c647adadfbbb9158f8a6a29b230d72c05
ThinkRobotics.in <50299210+thinkrobotics@users.noreply.github.com>
2019-07-22 12:39:27 +0530

    Mechanical Gripper for Robot Arm with MG996R Servo
    Various files related to tutorial on: https://thinkrobotics.in/blogs/learn/mechanical-gripper-for-robot-arm-with-mg996r-servo

---
+++ Mechanical Gripper  MG996R/gripper.ino
@@ -0,0 +1,25 @@
+#include<Servo.h>
+Servo claw;
+void setup()
+{
+  claw.attach(6);
+}
+
+void OpenClaw()
+{
+  claw.write(90);
+}
+
+void CloseClaw()
+{
+  claw.write(120);
+}
+
+void loop()
+{
+  OpenClaw();
+  delay(1000);
+  CloseClaw();
+  delay(1000);
+}
+
